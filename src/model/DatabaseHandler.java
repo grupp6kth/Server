dd69edd02fd2d75f1babc66ca92a04417e1e6ea7
Tom <tomkare@kth.se>
2016-05-18 10:43:35 +0200

    Updated DatabaseHandler and ScheduleExecutor

--- src/model/DatabaseHandler.java
+++ src/model/DatabaseHandler.java
@@ -59,11 +59,11 @@ public final class DatabaseHandler {
 
             while(rs.next()){
                 schedule.add(new ScheduledEvent(
-                        rs.getInt("device_id"),
-                        rs.getString("device_name"),
-                        rs.getString("start_date"),
-                        null,
-                        rs.getString("description")));
+                        rs.getInt("deviceID"),
+                        rs.getString("deviceName"),
+                        rs.getString("startDateTime").substring(0,16),
+                        rs.getString("endDateTime").substring(0,16),
+                        rs.getString("newDeviceStatus")));
             }
         }catch (NullPointerException | SQLException ex){
             ex.printStackTrace();
@@ -71,10 +71,183 @@ public final class DatabaseHandler {
             closeConnectionWithResultSet(conn, stmt, rs);
         }
 
-        //schedule.add(new ScheduledEvent(3, "UlrikasLampa", "2016-09-11 11:33", null, "ON"));
-        //schedule.add(new ScheduledEvent(4, "AlexandersLampa", "2016-09-11 11:53", "2016-09-11 11:55" , "OFF"));
-
         return new Schedule(schedule);
+    }
+
+    /**
+     * Sets startdate to enddate and inverts status, sets enddate to null
+     */
+    public void modifyEvent(ScheduledEvent scheduledEvent){
+        Connection conn = getConnection();
+        Statement stmt = null;
+        ResultSet rs = null;
+        String query = "SELECT * FROM scheduler.events ORDER BY startDateTime asc limit 1";
+
+        try{
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(query);
+            if(rs.next()){
+                if (rs.getString("newDeviceStatus").equals("ON")){
+                    query = "UPDATE scheduler.events SET startDateTime='" + rs.getString("endDateTime") + "', endDateTime=NULL, newDeviceStatus='OFF' WHERE (deviceID, startdateTime) = (" + scheduledEvent.getDeviceID() + ", '" + scheduledEvent.getStartDateTime() +"')";
+                }
+                else{
+                    query = "UPDATE scheduler.events SET startDateTime='" + rs.getString("endDateTime") + "', endDateTime=NULL, newDeviceStatus='ON' WHERE (deviceID, startdateTime) = (" + scheduledEvent.getDeviceID() + ", '" + scheduledEvent.getStartDateTime() +"')";
+                }
+                stmt.executeUpdate(query);
+            }
+        }catch (NullPointerException | SQLException ex){
+            ex.printStackTrace();
+        } finally {
+            closeConnectionWithResultSet(conn, stmt, rs);
+        }
+
+        return;
+    }
+
+    /**
+     * Insert new event
+     */
+    public void insertEvent(ScheduledEvent scheduledEvent){
+        Connection conn = getConnection();
+        Statement stmt = null;
+        String query;
+        if (scheduledEvent.getEndDateTime() != null) {
+            query = "INSERT INTO scheduler.events " + "VALUES ("
+                    + scheduledEvent.getDeviceID() + ", '"
+                    + scheduledEvent.getDeviceName() + "', '"
+                    + scheduledEvent.getStartDateTime() + "', '"
+                    + scheduledEvent.getEndDateTime() + "', '"
+                    + scheduledEvent.getNewDeviceStatus() + "')";
+        }
+        else{
+            query = "INSERT INTO scheduler.events " + "VALUES ("
+                    + scheduledEvent.getDeviceID() + ", '"
+                    + scheduledEvent.getDeviceName() + "', '"
+                    + scheduledEvent.getStartDateTime() + "', NULL, '"
+                    + scheduledEvent.getNewDeviceStatus() + "')";
+        }
+
+        try{
+            stmt = conn.createStatement();
+            stmt.executeUpdate(query);
+
+        }catch (NullPointerException | SQLException ex){
+            ex.printStackTrace();
+        }finally {
+            closeConnection(conn, stmt);
+        }
+
+        return;
+    }
+
+    /**
+     * Remove event
+     */
+    public void removeEvent(ScheduledEvent scheduledEvent){
+        Connection conn = getConnection();
+        Statement stmt = null;
+        String query = "DELETE FROM scheduler.events WHERE (deviceID, startDateTime) = (" + scheduledEvent.getDeviceID() + ", '" + scheduledEvent.getStartDateTime() + "')";
+
+        try{
+            stmt = conn.createStatement();
+            stmt.executeUpdate(query);
+
+        }catch (NullPointerException | SQLException ex){
+            ex.printStackTrace();
+        }finally {
+            closeConnection(conn, stmt);
+        }
+
+        return;
+    }
+
+    /**
+     * @return earliestStartDate from events table.
+     *
+     * Not really req since getEarliestEvent can get this info aswell.
+     */
+    public String getEarliestStartDate(){
+        Connection conn = getConnection();
+        Statement stmt = null;
+        ResultSet rs = null;
+        String query = "SELECT startDateTime FROM scheduler.events ORDER BY startDateTime asc limit 1";
+        String earliestStartDate = "";
+        try{
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(query);
+            if(rs.next()){
+                earliestStartDate = rs.getString("startDateTime");
+            }
+            rs.close();
+
+        }catch (NullPointerException | SQLException ex){
+            ex.printStackTrace();
+        }finally {
+            closeConnectionWithResultSet(conn, stmt, rs);
+        }
+
+        return earliestStartDate.substring(0,16);
+    }
+
+
+    /**
+     * @return earliestEvent by startdatetime from events table.
+     */
+    public ScheduledEvent getEarliestEvent(){
+        Connection conn = getConnection();
+        Statement stmt = null;
+        ResultSet rs = null;
+        String query = "SELECT * FROM scheduler.events ORDER BY startDateTime asc limit 1";
+        try{
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(query);
+            if(rs.next()){
+
+                ScheduledEvent event = new ScheduledEvent(
+                        rs.getInt("deviceID"),
+                        rs.getString("deviceName"),
+                        rs.getString("startDateTime"),
+                        rs.getString("endDateTime"),
+                        rs.getString("newDeviceStatus"));
+                rs.close();
+                return event;
+            }
+        }
+        catch (NullPointerException | SQLException ex){
+            ex.printStackTrace();
+        }finally {
+            closeConnectionWithResultSet(conn, stmt, rs);
+        }
+        //returns event with nulls if object is empty. Solution?
+        //return new ScheduledEvent(404, null, null, null, null);
+        return null;
+    }
+
+    /**
+     * @return earliestEndDate from events table
+     * substring used at end to get format yyyy-mm-dd hh-mm
+     */
+    public String getEarliestEndDate(){
+        Connection conn = getConnection();
+        Statement stmt = null;
+        ResultSet rs = null;
+        String query = "SELECT endDateTime FROM scheduler.events ORDER BY endDateTime asc limit 1";
+        String earliestStartDate = "";
+        try{
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(query);
+            if(rs.next()){
+                earliestStartDate = rs.getString("endDateTime");
+            }
+            rs.close();
+
+        }catch (NullPointerException | SQLException ex){
+            ex.printStackTrace();
+        }finally {
+            closeConnectionWithResultSet(conn, stmt, rs);
+        }
+
+        return earliestStartDate.substring(0,16);
     }
 
     /**
--- src/model/ScheduleExecutor.java
+++ src/model/ScheduleExecutor.java
@@ -1,9 +1,53 @@
 package model;
 
+
+import DTO.ControlDevice;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  * This class will execute scheduled events
  */
 public class ScheduleExecutor {
     private TelldusAPI telldusAPI = TelldusAPI.getInstance();
     private DatabaseHandler DBHandler = DatabaseHandler.getInstance();
+
+
+
+    /**
+     * Executes event with telldusAPI and SQL queries with DBhandler.
+     */
+    public void executeEvent(){
+        if (eventTimePassed(DBHandler.getEarliestEvent().getStartDateTime()) && (DBHandler.getEarliestEvent().getEndDateTime() == null) ){
+
+            telldusAPI.changeDeviceStatus(new ControlDevice(DBHandler.getEarliestEvent().getDeviceID())); //only switches devices.
+            DBHandler.removeEvent(DBHandler.getEarliestEvent());  //remove event from DB after execution
+
+        }
+        else if(eventTimePassed(DBHandler.getEarliestEvent().getStartDateTime()) && (DBHandler.getEarliestEvent().getEndDateTime() != null)){
+
+            telldusAPI.changeDeviceStatus(new ControlDevice(DBHandler.getEarliestEvent().getDeviceID())); //only switches devices.
+            DBHandler.modifyEvent(DBHandler.getEarliestEvent()); //modify current event
+        }
+    }
+
+
+    /**
+     * @param eventTime time of event with format yyyy-MM-dd HH:mm
+     * @return true if currentTime >= eventTime (If eventTime has passed), else false.
+     */
+    private boolean eventTimePassed (String eventTime){
+        String currentTime = new SimpleDateFormat("yyyyMMddHHmm").format(Calendar.getInstance().getTime());
+        eventTime = eventTime.substring(0,4).concat(eventTime.substring(5,7)).concat(eventTime.substring(8,10)).concat(eventTime.substring(11,13)).concat(eventTime.substring(14,16));
+
+        if(Integer.parseInt(currentTime) >= Integer.parseInt(eventTime)){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
 }
